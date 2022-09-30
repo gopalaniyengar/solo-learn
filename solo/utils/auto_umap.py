5132d2ad606b6203bc42b08ea9bef94e1e0c9ad0
Gopalan Iyengar <63103249+gopalaniyengar@users.noreply.github.com>
2022-09-30 18:26:49 +0530

    Add style and content projections - Online UMAP

--- solo/utils/auto_umap.py
+++ solo/utils/auto_umap.py
@@ -140,7 +140,11 @@ class AutoUMAP(Callback):
         """
 
         device = module.device
-        data = []
+        bb_feats = []
+        bb_style_feats = []
+        style_proj_feats = []
+        cont_proj_feats = []
+
         Y = []
 
         # set module to eval model and collect all feature representations
@@ -150,60 +154,80 @@ class AutoUMAP(Callback):
                 x = x.to(device, non_blocking=True)
                 y = y.to(device, non_blocking=True)
 
-                feats = module(x)["feats"]
+                all_feats_dict = module(x)
+                feat = all_feats_dict["feats"]
+                style_feat = all_feats_dict["style_feats"]
+                style_proj = all_feats_dict["s"]
+                cont_proj = all_feats_dict["z"]
 
-                feats = gather(feats)
+                feat = gather(feat)
+                style_feat = gather(style_feat)
+                style_proj = gather(style_proj)
+                cont_proj = gather(cont_proj)
                 y = gather(y)
 
-                data.append(feats.cpu())
+                bb_feats.append(feat.cpu())
+                bb_style_feats.append(style_feat.cpu())
+                style_proj_feats.append(style_proj.cpu())
+                cont_proj_feats.append(cont_proj.cpu())
+                # data.append(feats.cpu())
                 Y.append(y.cpu())
+
         module.train()
+        umap_keys = ['backbone_features','style_features','style_projections','content_projections']
+        palettes  = ['hls', 'icefire', 'viridis', 'Paired']
+        sns.set_palette("dark")
 
-        if trainer.is_global_zero and len(data):
-            data = torch.cat(data, dim=0).numpy()
-            Y = torch.cat(Y, dim=0)
-            num_classes = len(torch.unique(Y))
-            Y = Y.numpy()
+        for idxx, data in enumerate([bb_feats, bb_style_feats, style_proj_feats, cont_proj_feats]):
+            if trainer.is_global_zero and len(data):
+                data = torch.cat(data, dim=0).numpy()
+                Y = torch.cat(Y, dim=0)
+                num_classes = len(torch.unique(Y))
+                Y = Y.numpy()
 
-            data = umap.UMAP(n_components=2).fit_transform(data)
+                data = umap.UMAP(n_components=2).fit_transform(data)
 
-            # passing to dataframe
-            df = pd.DataFrame()
-            df["feat_1"] = data[:, 0]
-            df["feat_2"] = data[:, 1]
-            df["Y"] = Y
-            plt.figure(figsize=(16, 9))
-            ax = sns.scatterplot(
-                x="feat_1",
-                y="feat_2",
-                hue="Y",
-                palette=sns.color_palette(self.color_palette, num_classes),
-                data=df,
-                legend="full",
-                alpha=0.3,
-            )
-            ax.set(xlabel="", ylabel="", xticklabels=[], yticklabels=[])
-            ax.tick_params(left=False, right=False, bottom=False, top=False)
-
-            # manually improve quality of imagenet umaps
-            if num_classes > 100:
-                anchor = (0.5, 1.8)
-            else:
-                anchor = (0.5, 1.35)
-
-            plt.legend(loc="upper center", bbox_to_anchor=anchor, ncol=math.ceil(num_classes / 10))
-            plt.tight_layout()
-
-            if isinstance(trainer.logger, pl.loggers.WandbLogger):
-                wandb.log(
-                    {"validation_umap": wandb.Image(ax)},
-                    commit=False,
+                # passing to dataframe
+                df = pd.DataFrame()
+                df["feat_1"] = data[:, 0]
+                df["feat_2"] = data[:, 1]
+                df["Y"] = Y
+                plt.figure(figsize=(16, 9))
+                ax = sns.scatterplot(
+                    x="feat_1",
+                    y="feat_2",
+                    hue="Y",
+                    palette=sns.color_palette(palettes[idxx], num_classes),
+                    data=df,
+                    legend="full",
+                    alpha=0.3,
                 )
+                ax.set(xlabel="", ylabel="", xticklabels=[], yticklabels=[])
+                ax.tick_params(left=False, right=False, bottom=False, top=False)
 
-            # save plot locally as well
-            epoch = trainer.current_epoch  # type: ignore
-            plt.savefig(self.path / self.umap_placeholder.format(epoch))
-            plt.close()
+                # manually improve quality of imagenet umaps
+                if num_classes > 100:
+                    anchor = (0.5, 1.8)
+                else:
+                    anchor = (0.5, 1.35)
+
+                plt.legend(loc="upper center", bbox_to_anchor=anchor, ncol=math.ceil(num_classes / 10))
+                plt.tight_layout()
+
+                if isinstance(trainer.logger, pl.loggers.WandbLogger):
+                    # wandb.log(
+                    #     {"validation_umap": wandb.Image(ax)},
+                    #     commit=False,
+                    # )
+                    wandb.log(
+                        {umap_keys[idxx]: wandb.Image(ax)},
+                        commit=False,
+                    )
+
+                # save plot locally as well
+                epoch = trainer.current_epoch  # type: ignore
+                # plt.savefig(self.path / self.umap_placeholder.format(epoch))
+                plt.close()
 
     def on_validation_end(self, trainer: pl.Trainer, module: pl.LightningModule):
         """Tries to generate an up-to-date UMAP visualization of the features
