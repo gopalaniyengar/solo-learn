91ee3c580844e6cfe81c402df5fd039bba8b6e37
Gopalan Iyengar <63103249+gopalaniyengar@users.noreply.github.com>
2022-09-30 17:25:04 +0530

    Update auto_umap.py

--- solo/utils/auto_umap.py
+++ solo/utils/auto_umap.py
@@ -205,84 +205,6 @@ class AutoUMAP(Callback):
             plt.savefig(self.path / self.umap_placeholder.format(epoch))
             plt.close()
 
-    def plot_train_umap(self, trainer: pl.Trainer, module: pl.LightningModule):
-        """Produces a UMAP visualization by forwarding all data of the
-        first training dataloader through the module.
-
-        Args:
-            trainer (pl.Trainer): pytorch lightning trainer object.
-            module (pl.LightningModule): current module object.
-        """
-
-        device = module.device
-        data = []
-        Y = []
-
-        # set module to eval model and collect all feature representations
-        module.eval()
-        with torch.no_grad():
-            for x, y in trainer.train_dataloaders[0]:
-                x = x.to(device, non_blocking=True)
-                y = y.to(device, non_blocking=True)
-
-                feats = module(x)["feats"]
-
-                feats = gather(feats)
-                y = gather(y)
-
-                data.append(feats.cpu())
-                Y.append(y.cpu())
-        module.train()
-
-        if trainer.is_global_zero and len(data):
-            data = torch.cat(data, dim=0).numpy()
-            Y = torch.cat(Y, dim=0)
-            num_classes = len(torch.unique(Y))
-            Y = Y.numpy()
-
-            data = umap.UMAP(n_components=2).fit_transform(data)
-
-            # passing to dataframe
-            df = pd.DataFrame()
-            df["feat_1"] = data[:, 0]
-            df["feat_2"] = data[:, 1]
-            df["Y"] = Y
-            plt.figure(figsize=(16, 9))
-            # plette = sns.color_palette(self.color_palette, num_classes)
-            plette = sns.color_palette('icefire', num_classes)        
-            ax = sns.scatterplot(
-                x="feat_1",
-                y="feat_2",
-                hue="Y",
-                palette=plette,
-                data=df,
-                legend="full",
-                alpha=0.3,
-            )
-            ax.set(xlabel="", ylabel="", xticklabels=[], yticklabels=[])
-            ax.tick_params(left=False, right=False, bottom=False, top=False)
-
-            # manually improve quality of imagenet umaps
-            if num_classes > 100:
-                anchor = (0.5, 1.8)
-            else:
-                anchor = (0.5, 1.35)
-
-            plt.legend(loc="upper center", bbox_to_anchor=anchor, ncol=math.ceil(num_classes / 10))
-            plt.tight_layout()
-
-            if isinstance(trainer.logger, pl.loggers.WandbLogger):
-                wandb.log(
-                    {"train_umap": wandb.Image(ax)},
-                    commit=False,
-                )
-
-            # save plot locally as well
-            epoch = trainer.current_epoch  # type: ignore
-            plt.savefig(self.path / self.umap_placeholder.format('train'+str(epoch)))
-            plt.close()
-
-
     def on_validation_end(self, trainer: pl.Trainer, module: pl.LightningModule):
         """Tries to generate an up-to-date UMAP visualization of the features
         at the end of each validation epoch.
@@ -294,7 +216,6 @@ class AutoUMAP(Callback):
         epoch = trainer.current_epoch  # type: ignore
         if epoch % self.frequency == 0 and not trainer.sanity_checking:
             self.plot(trainer, module)
-            #self.plot_train_umap(trainer, module)
 
 
 class OfflineUMAP:
